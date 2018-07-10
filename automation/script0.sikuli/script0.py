52ee30392b36cac69aaca1859bc09f9f4115d046
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-10 16:17:13 +0100

    add first attempt at getting a screen capture - not quite working in the capture command

--- automation/script0.sikuli/script0.py
+++ automation/script0.sikuli/script0.py
@@ -1,4 +1,20 @@
 click("1531232060307.png")
 type("Xcode\n")
+wait("1531234322045.png")
+type("\n")
+wait("1531234862082.png")
+type("<", Key.CMD)
+click("1531235181943.png")
+screen = Screen()
+file = screen.capture(Region(1251,221,398,389))
+print("Saved screen as "+file)
+
+
+
+
+
+
+
+
 
 
