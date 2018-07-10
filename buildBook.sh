716e239e993be70b22bf15630f0e0f8d21391c68
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-10 14:45:53 +0100

    still generate the old latex style pdf

--- buildBook.sh
+++ buildBook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -c style/gitHubStyle.css -o foo.html
+pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -o foo.pdf
