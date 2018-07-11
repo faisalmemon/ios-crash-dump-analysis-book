7c2111fbba9a87900fc8cf5d3d57094291e9f0de
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-11 09:24:51 +0100

    update code to publish book

--- buildBook.sh
+++ buildBook.sh
@@ -6,5 +6,8 @@ pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone -
 mkdir -p docs
 cp foo.html docs/index.html
 git add docs/index.html
+rm -rf docs/screenshots
+cp -pr screenshots docs/screenshots
+git add docs/screenshots
 git commit -m'update published book on github pages'
 git push
