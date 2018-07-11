392b9174882e5c02382a567abb8fd1c51af51c8c
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-11 09:10:05 +0100

    add publishing code

--- buildBook.sh
+++ buildBook.sh
@@ -2,3 +2,9 @@
 
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -c style/gitHubStyle.css -o foo.html
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -o foo.pdf
+
+mkdir -p docs
+cp foo.html docs/index.html
+git add docs/index.html
+git commit -m'update published book on github pages'
+git push
