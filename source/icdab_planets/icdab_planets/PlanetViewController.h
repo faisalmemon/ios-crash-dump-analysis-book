ec8d18950e2e1b2be21a360ddbee253636e33c73
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-16 10:01:09 +0100

    clean up project

---
+++ source/icdab_planets/icdab_planets/PlanetViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlanetViewController.h
+//  icdab_planets
+//
+//  Created by Faisal Memon on 16/07/2018.
+//  Copyright © 2018 Faisal Memon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanetViewController : UIViewController
+
+@end
---
+++ source/icdab_planets/icdab_planets/PlanetViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlanetViewController.m
+//  icdab_planets
+//
+//  Created by Faisal Memon on 16/07/2018.
+//  Copyright © 2018 Faisal Memon. All rights reserved.
+//
+
+#import "PlanetViewController.h"
+
+@interface PlanetViewController ()
+
+@end
+
+@implementation PlanetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
