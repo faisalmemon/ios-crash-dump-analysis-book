09c4c42212a603598f21677fa0200d48166a837f
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-25 14:37:39 +0100

    detect usage of you

--- buildBook.sh
+++ buildBook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo Usages of "you" in the narrative
+grep --color you $(cat pageOrder.txt) -c
+echo
+grep --color you $(cat pageOrder.txt)
+
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -c style/gitHubStyle.css -o foo.html
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -o foo.pdf
 #pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib -o foo.docx  ## This gets you a docx file which you can style edit, save and then run the reference-doc command instead to use the style of that document.
