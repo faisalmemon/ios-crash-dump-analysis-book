10fbdf1f759b33a4dc335383f793116405aa79fa
Faisal Memon <faisalmemonuk@yahoo.com>
2018-07-25 17:18:13 +0100

    fix build script

--- buildBook.sh
+++ buildBook.sh
@@ -6,7 +6,8 @@ echo
 grep --color you $(cat pageOrder.txt| grep -v Introduction)
 
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -c style/gitHubStyle.css -o foo.html
-pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -o foo.pdf
+pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc --template=style/simple.latex -o foo.pdf
+
 #pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib -o foo.docx  ## This gets you a docx file which you can style edit, save and then run the reference-doc command instead to use the style of that document.
 pandoc $(cat pageOrder.txt) pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --reference-doc=style/referenceWordDocumentTemplate.docx -o foo.docx
 /Applications/calibre.app/Contents/console.app/Contents/MacOS/ebook-convert foo.docx foo.epub
