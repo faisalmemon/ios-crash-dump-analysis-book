2ee92ed3c45673dbc705fb1d90097bf3895b1c6c
Faisal Memon <faisalmemonuk@yahoo.com>
2018-08-01 16:31:04 +0100

    add test to test suite

--- source/automation/runBuilds.sh
+++ source/automation/runBuilds.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-testDevice=iPad_Pro_9_7_inch
 workspaceFile=../icdab.xcworkspace
+
+# In your Devices manager, set the name for your attached iPad as follows
+testDevice=iPad_Pro_9_7_inch
+
+# Hardware Testing on iPad Pro
 testSuite="icdab_aslTests icdab_aslUITests icdab_asTests icdab_asUITests icdab_planets_facadeTests icdab_planets_facadeUITests icdab_planets_stlTests icdab_planets_stlUITests icdab_planetsTests icdab_planetsUITests icdab_wrapTests icdab_wrapUITests"
+
+# Simulator Testing on Mac
+logicTestingTestSuite="icdab_thread_test"
 
 echo We change into the script directory in order to execute using relative paths for resources
 cd "${BASH_SOURCE%/*}" || exit
@@ -13,3 +20,8 @@ do
 	xcodebuild test -workspace $workspaceFile -scheme $testScheme -destination name=$testDevice
 done
 
+for testScheme in $logicTestingTestSuite
+do
+	echo ++ Scheme ++ $testScheme
+	xcodebuild test -workspace $workspaceFile -scheme $testScheme -destination platform=macOS
+done
