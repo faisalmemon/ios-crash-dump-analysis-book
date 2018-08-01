1866fbec2aabf308f70dcb67b210661f5343a8ac
Faisal Memon <faisalmemonuk@yahoo.com>
2018-08-01 20:02:09 +0100

    add SIP check

--- source/tfpexample/tfpexample/build.sh
+++ source/tfpexample/tfpexample/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+csrutil status | grep disabled
+wasDisabled=$?
+
+if [[ $wasDisabled == "1" ]]
+then
+	echo We need System Integrity Protection Disabled
+	exit 1
+fi
+
 mkdir -p generated
 
 gcc tfpexample.c -sectcreate __TEXT __info_plist ./Info.plist -o generated/tfpexample -framework Security -framework CoreFoundation
