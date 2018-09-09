45077617085b356ea9813a2d2b2690996126db6a
Faisal Memon <faisalmemonuk@yahoo.com>
2018-09-09 09:16:29 +0100

    add bridging

---
+++ source/icdab_edge/icdab_edge/CrashHelper2.m
@@ -0,0 +1,9 @@
+//
+//  CrashHelper2.m
+//  icdab_edge
+//
+//  Created by Faisal Memon on 09/09/2018.
+//  Copyright © 2018 Faisal Memon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
