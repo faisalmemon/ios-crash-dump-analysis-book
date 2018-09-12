03cd14348df74f760121728a2092b7ea3c1e3649
Faisal Memon <faisalmemonuk@yahoo.com>
2018-09-12 15:23:27 +0100

    add a watch project for showing a broken payload

---
+++ source/icdab_broken_payload/icdab_broken_payloadTests/icdab_broken_payloadTests.swift
@@ -0,0 +1,36 @@
+//
+//  icdab_broken_payloadTests.swift
+//  icdab_broken_payloadTests
+//
+//  Created by Faisal Memon on 12/09/2018.
+//  Copyright © 2018 Faisal Memon. All rights reserved.
+//
+
+import XCTest
+@testable import icdab_broken_payload
+
+class icdab_broken_payloadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
