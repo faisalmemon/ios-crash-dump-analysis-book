b1214b24d530a436dc40d1604ea2cd2171411811
Faisal Memon <faisalmemonuk@yahoo.com>
2018-09-14 20:12:01 +0100

    make comma handling robust

--- commatrademark.sh
+++ commatrademark.sh
@@ -2,4 +2,17 @@
 
 # Take the index file and extract trademark references, and construct a comma separated list.
 
+# foo.idx is the latex generated index (from the previous build)
+# awk using $2 is to get the trademark index detail item (not the plain word trademark)
+# awk using $1 is to chop out trailing text from the detail item which is the textual trademark
+# sort is to get unique entries
+# paste is to convert newline separated items into a comma separated list
+# sed with /, / is to put a space after the commas
+# sed with $/\./ is to put a period at the end of the line
+
+# If there was no previous build, leave the trademark file empty by producing empty output
+
+if [[ -f foo.idx ]] 
+then
 grep trademark foo.idx | awk -F'!' '{print $2}' | awk -F"}" '{print $1}' | sort -u | paste -s -d, - | sed -e 's/,/, /g' | sed -e 's/$/\./'
+fi
