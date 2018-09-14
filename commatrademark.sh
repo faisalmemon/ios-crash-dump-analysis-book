5f92e5dbd25d38bd72856d57628c6caf485e0cf8
Faisal Memon <faisalmemonuk@yahoo.com>
2018-09-14 20:05:58 +0100

    fix commatrademark

--- commatrademark.sh
+++ commatrademark.sh
@@ -2,4 +2,4 @@
 
 # Take the index file and extract trademark references, and construct a comma separated list.
 
-grep trademark foo.idx | awk -F'!' '{print $2}' | awk -F"}" '{print $1}' | sort -u | paste -s -d, - | sed -e 's/,/, /g'
+grep trademark foo.idx | awk -F'!' '{print $2}' | awk -F"}" '{print $1}' | sort -u | paste -s -d, - | sed -e 's/,/, /g' | sed -e 's/$/\./'
