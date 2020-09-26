24df8351087bfbd3d42da1a6839ca669799942a9
Faisal Memon <faisalmemonuk@yahoo.com>
2020-09-26 15:21:40 +0100

    clean up logging messages and print progress

--- buildBook.sh
+++ buildBook.sh
@@ -36,8 +36,6 @@ rm foo.$langName.*
 filesToProcess=$(./get_markdown_for_lang.sh -l $langName $(cat frontPages.txt mainPages.txt))
 latexFilesToProcess=$(./get_markdown_for_lang.sh -l $langName $(cat frontPages_latex.txt mainPages.txt))
 
-echo filesToProcess is $filesToProcess
-echo latexFilesToProcess is $latexFilesToProcess
 
 # We allow first person in the preface and second person in the Introduction.
 # Elsewhere it is third person only.
@@ -57,15 +55,25 @@ do
 
 done
 
+echo Processing foo.$langName.html
 pandoc $filesToProcess pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc -c style/gitHubStyle.css -o foo.$langName.html
+echo Processing boo.$langName.latex
 pandoc $latexFilesToProcess pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc --template=style/styleToCreateIndex.latex -V documentclass=book -o boo.$langName.latex
+
+echo Indexing pass 0
 pdflatex boo.$langName.latex > boo.$langName.pass.0.log
+echo Indexing pass 1
 pdflatex boo.$langName.latex > boo.$langName.pass.1.log
+echo Indexing pass 2
 pdflatex boo.$langName.latex > boo.$langName.pass.2.log
 
+echo Processing foo.$langName.epub
 pandoc $filesToProcess pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --toc --css=style/ebook.css -o foo.$langName.epub
 
+echo Processing foo.$langName.docx
 pandoc $filesToProcess pandocMetaData.yaml -f markdown+smart --standalone --bibliography bibliography.bib --reference-doc=style/referenceWordDocumentTemplate.docx -o foo.$langName.docx
+
+echo Constructing github pages
 
 mkdir -p docs/$langName
 cp foo.$langName.html docs/$langName/index.html
@@ -73,6 +81,8 @@ git add docs/$langName/index.html
 rm -rf docs/$langName/screenshots docs/$langName/style
 cp -pr screenshots docs/$langName/screenshots
 cp -pr style docs/$langName/style
+
+echo Checking in github pages changes
 git add docs/$langName/screenshots docs/$langName/style
 git commit -m"update published $langName book on github pages"
 git push
