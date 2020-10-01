4f8d2944a013579d2e1570d291f390806c56eaba
Faisal Memon <faisalmemonuk@yahoo.com>
2020-10-01 10:39:49 +0100

    modernise icdab_sample and remove image picker code because the original problem we were exposing with camera permissions is now legacy from 9.4.1

--- source/icdab_sample/icdab_sample/ViewController.swift
+++ source/icdab_sample/icdab_sample/ViewController.swift
@@ -9,27 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func takePhotoButtonAction(_ sender: Any) {
-        takeCameraPhoto()
-    }
     
     @IBOutlet weak var imageViewOutlet: UIImageView!
     
     @IBOutlet weak var macAddressLabelOutlet: UILabel!
     
-    func takeCameraPhoto() {
-        if UIImagePickerController
-            .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-            // Use Xcode 9.4.1 to see it enter here
-            // Xcode 10.0 will skip over
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         let macAddress = MacAddress()
@@ -46,16 +30,6 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UIImagePickerControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            imageViewOutlet.image = pickedImage
-        }
-        picker.dismiss(animated: true, completion: nil)
-    }
-}
-
 extension ViewController: UINavigationControllerDelegate {
     
 }
-
