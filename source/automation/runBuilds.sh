6347fd7bf7123dd91fdfba486d46747ff1327769
Faisal Memon <faisalmemonuk@yahoo.com>
2020-10-03 18:21:16 +0100

    add camera app to the workspace and automated tests

--- source/automation/runBuilds.sh
+++ source/automation/runBuilds.sh
@@ -10,7 +10,7 @@ testDevice=iPod_touch_7_gen
 testAppleTv=apple_tv
 
 # Hardware Testing on iPad Pro
-testSuite="icdab_edgeTests icdab_aslTests icdab_aslUITests icdab_asTests icdab_asUITests icdab_planets_facadeTests icdab_planets_facadeUITests icdab_planets_stlTests icdab_planets_stlUITests icdab_planetsTests icdab_planetsUITests icdab_wrapTests icdab_wrapUITests"
+testSuite="icdab_edgeTests icdab_aslTests icdab_aslUITests icdab_asTests icdab_asUITests icdab_cameraUITests icdab_planets_facadeTests icdab_planets_facadeUITests icdab_planets_stlTests icdab_planets_stlUITests icdab_planetsTests icdab_planetsUITests icdab_wrapTests icdab_wrapUITests"
 
 # Simulator Testing on Mac
 logicTestingTestSuite="icdab_thread_test"
