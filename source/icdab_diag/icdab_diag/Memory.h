227623db1cf058017487d0b1e2f6bc67008ec139
Faisal Memon <faisalmemonuk@yahoo.com>
2020-10-21 14:41:34 +0100

    add to metric kit sample app

--- source/icdab_diag/icdab_diag/Memory.h
+++ source/icdab_diag/icdab_diag/Memory.h
@@ -12,6 +12,8 @@
 
 @interface Memory : NSObject
 
+- (void)dereferenceBadPointer;
+
 @end
 
 
--- source/icdab_diag/icdab_diag/ViewController.swift
+++ source/icdab_diag/icdab_diag/ViewController.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+import MetricKit
 
 class ViewController: UIViewController {
 
     let memory = Memory()
+    let metrics = MXMetricManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        metrics.add(self)
+        for item in metrics.pastDiagnosticPayloads {
+            print(item)
+        }
+        for item in metrics.pastPayloads {
+            print(item)
+        }
     }
 
+    @IBAction func dereferenceBadPointerButtonAction(_ sender: UIButton) {
+        memory.dereferenceBadPointer()
+    }
+    
+}
 
+extension ViewController: MXMetricManagerSubscriber {
+    func didReceive(_ payloads: [MXMetricPayload]) {
+        for payload in payloads {
+            print(payload)
+        }
+    }
+    
+    func didReceive(_ payloads: [MXDiagnosticPayload]) {
+        for payload in payloads {
+            print(payload)
+        }
+    }
 }
 
